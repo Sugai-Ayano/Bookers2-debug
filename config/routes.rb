0ac73ae7734d88edb8172864a487ecdd4847e5c4
Sugai-Ayano <b3w41067a@gmail.com>
2021-01-10 04:55:50 +0000

    [Start]課題8

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    resistrations: 'devise/resistrations',
+  }
+
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
   get 'home/about', to: 'homes#about', as: 'home_about'
   root to: 'homes#top'
 end
+
